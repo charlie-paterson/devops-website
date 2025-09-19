0d1eab8ce9321615abfffd6fb17138aa961775fa
charlie-paterson <charlie_paterson@rocketmail.com>
2025-09-19 14:05:42 +0100

    change

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,43 @@
-caches.open("v1").then(cache => {
-  return cache.addAll([
-    "/",
-    "/index.html",
-    "/style.css",
-    "/script.js",
-    "/images/car1.png",
-    "/images/car2.png"
-  ]);
+const CACHE_NAME = 'f1-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  '/images/icon-72.png',
+  '/images/icon-96.png',
+  '/images/icon-128.png',
+  '/images/icon-192.png',
+  '/images/icon-512.png'
+  // Add any other images you want cached
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+// Fetch event (serve from cache if offline)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('/index.html')) // fallback
+  );
 });
