0ed7ac09dc224a8869b85e2f44d675e177293d40
Charlie Paterson <136974885+charlie-paterson@users.noreply.github.com>
2025-09-25 10:36:58 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/12.3.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/12.3.0/firebase-messaging.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyBhc7dAgsepoTZMhHK03VDC2rYK4VOGvtY",
@@ -7,16 +7,16 @@ firebase.initializeApp({
   projectId: "f1-hub-32f3f",
   storageBucket: "f1-hub-32f3f.firebasestorage.app",
   messagingSenderId: "386786917585",
-  appId: "1:386786917585:web:6a4da76552d4bcacaf59f0",
-  measurementId: "G-N20CZ0XQZ9"
+  appId: "1:386786917585:web:6a4da76552d4bcacaf59f0"
 });
 
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log("Background message received:", payload);
-  self.registration.showNotification(payload.notification.title, {
-    body: payload.notification.body,
-    icon: "/images/icon-180.png"
+  console.log('Background message received:', payload);
+  self.registration.showNotification(payload.notification?.title, {
+    body: payload.notification?.body,
+    icon: '/f1-hub-32f3f/images/icon-180.png'
   });
 });
+
